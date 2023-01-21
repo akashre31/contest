a26dc7ca4406bca35d4cef8a6371d3a69e24ab18
akashre31 <116092315+akashre31@users.noreply.github.com>
2023-01-21 20:55:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let nameinp =document.getElementById('name');
+let emailinp=document.getElementById("email");
+let passinp=document.getElementById("password");
+let confirminp=document.getElementById("cofirmpassword");
+let submit=document.getElementById("submit");
+
+submit.addEventListener("click",validation);
+ 
+function validation(){
+    let nameval = nameinp.value;
+    console.log(nameval);
+    if ()
+}
